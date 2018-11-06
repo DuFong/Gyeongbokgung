0923157aa9467868f28a9a7ee50c9604a77eb1b5
YEONJAE-PC\yeonjae <duswo46@naver.com>
2018-11-06 11:08:01 +0900

    [add] 회원가입, 로그인

---
+++ app/src/main/java/gyeongbokgung/kbsccoding/com/gyeongbokgung/SignupActivity.java
@@ -0,0 +1,233 @@
+package gyeongbokgung.kbsccoding.com.gyeongbokgung;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    private static String IP_ADDRESS = "192.168.123.109";
+    // private static String TAG = "phptest";
+
+    @BindView(R.id.et_userid)
+    EditText mUserid;
+    @BindView(R.id.et_username)
+    EditText mUsername;
+    @BindView(R.id.et_password)
+    EditText mPassword;
+    @BindView(R.id.btn_signup)
+    Button mSignup;
+    @BindView(R.id.tv_login)
+    TextView mLogin;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.btn_signup)
+    void signup(){
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+        mSignup.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.Theme_AppCompat_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String userID = mUserid.getText().toString();
+        String userName = mUsername.getText().toString();
+        String userPassword = mPassword.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+        InsertData task = new InsertData();
+        task.execute("http://" + IP_ADDRESS + "/insert.php", userID,userPassword,userName);
+
+
+
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+
+        mSignup.setEnabled(false);
+    }
+    class InsertData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(SignupActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+
+            Log.d(TAG, "POST response  - " + result);
+
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String userID = (String)params[1];
+            String userPassword = (String)params[2];
+            String userName = (String)params[3];
+
+            String serverURL = (String)params[0];
+            String postParameters = "userID=" + userID + "&userPassword=" + userPassword+ "&userName=" + userName;
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(50000);
+                httpURLConnection.setConnectTimeout(50000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                onSignupFailed();
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
+
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String userID = mUserid.getText().toString();
+        String userName = mUsername.getText().toString();
+        String userPassword = mPassword.getText().toString();
+
+        if (userID.isEmpty() || userID.length() < 3) {
+            mUserid.setError("at least 3 characters");
+            valid = false;
+        } else {
+            mUserid.setError(null);
+        }
+
+        if (userName.isEmpty() || userName.length() < 3) {
+            mUsername.setError("at least 3 characters");
+            valid = false;
+        } else {
+            mUsername.setError(null);
+        }
+
+        if (userPassword.isEmpty() || userPassword.length() < 4 || userPassword.length() > 10) {
+            mPassword.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+    public void onSignupSuccess() {
+        mSignup.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Signup failed", Toast.LENGTH_LONG).show();
+        mSignup.setEnabled(true);
+    }
+
+    @OnClick(R.id.tv_login)
+    void login(){
+        // Finish the registration screen and return to the Login activity
+        finish();
+    }
+}
