819c0679a3fb5191f65600d24d8d7ebda094b7aa
YEONJAE-PC\yeonjae <duswo46@naver.com>
2018-11-10 17:49:43 +0900

    [add] scoreActivity (connect QUEST DB)

--- app/src/main/java/gyeongbokgung/kbsccoding/com/gyeongbokgung/LoginActivity.java
+++ app/src/main/java/gyeongbokgung/kbsccoding/com/gyeongbokgung/LoginActivity.java
@@ -303,7 +303,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public void onLoginSuccess() {
         mLogin.setEnabled(true);
-        Intent intent = new Intent(getApplicationContext(), RankingActivity.class);
+        Intent intent = new Intent(getApplicationContext(), ScoreActivity.class);
         Log.d(TAG,"~~~nowID"+nowPerson.getMember_id());
         String nid= nowPerson.getMember_id();
         Log.d(TAG,"~~~~nid:"+nid);
