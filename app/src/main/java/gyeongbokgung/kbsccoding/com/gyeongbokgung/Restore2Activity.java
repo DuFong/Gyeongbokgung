e6cf305dc1125b84571b36ab852e07b5ea918389
이영재 <>
2018-11-11 18:35:59 +0900

    [add] RestoreActivity 2+3

---
+++ app/src/main/java/gyeongbokgung/kbsccoding/com/gyeongbokgung/Restore2Activity.java
@@ -0,0 +1,13 @@
+package gyeongbokgung.kbsccoding.com.gyeongbokgung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Restore2Activity extends AppCompatActivity {
+    // 답이 2개인 문제에 대한 RestoreActivity.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restore2);
+    }
+}
