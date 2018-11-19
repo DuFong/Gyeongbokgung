bb7072f878063e7168a9e906b923363ffbef9749
이영재 <>
2018-11-19 22:08:41 +0900

    [add] after_ending marker

--- app/src/main/java/gyeongbokgung/kbsccoding/com/gyeongbokgung/AfterEndingActivity.java
+++ app/src/main/java/gyeongbokgung/kbsccoding/com/gyeongbokgung/AfterEndingActivity.java
@@ -1,19 +1,27 @@
 package gyeongbokgung.kbsccoding.com.gyeongbokgung;
 
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class AfterEndingActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class AfterEndingActivity extends FragmentActivity implements OnMapReadyCallback{
     GoogleMap mMap;
+
+    private long lastTimeClickMarker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,21 +39,39 @@ public class AfterEndingActivity extends AppCompatActivity implements OnMapReady
         LatLng chapter1 = new LatLng(37.578526, 126.976993);
         LatLng chapter0 = new LatLng(37.575996, 126.976917);
 
-        makeMarker(chapter0,"광화문");
-        makeMarker(chapter1,"근정전");
+        onAddMarker(chapter1,"근정전");
+        onAddMarker(chapter0, "광화문");
 
-        //카메라를 서울 위치로 옮긴다.
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(mDefaultLocation));
-    }
+        //카메라를 경복궁 위치로 옮긴다.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation,14));
+        }
 
-    private void makeMarker(LatLng location, String str){
+    private void onAddMarker(LatLng location, String str){
         MarkerOptions makerOptions = new MarkerOptions();
         makerOptions
                 .position(location)
                 .title(str);
 
-        mMap.addMarker(makerOptions);
+        this.mMap.addMarker(makerOptions).showInfoWindow();
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if (marker.getTitle().equals("근정전")) {
+
+                Log.d(marker.getTitle(), "김현규대가리");
+                Intent intent = new Intent(getApplicationContext(), RankingActivity.class);
+                startActivity(intent);
+            }
+                Log.d(marker.getTitle(),"김현규대가리");
+
+                //Toast.makeText(AfterEndingActivity.this, "해당 전각을 한 번 더 클릭하시면 해당 설명으로 이동합니다.", Toast.LENGTH_SHORT);
+                return false;
+            }
+        });
+
     }
+
 
     @Override
     public void onBackPressed() {
@@ -67,4 +93,7 @@ public class AfterEndingActivity extends AppCompatActivity implements OnMapReady
         builder.show();
         // super.onBackPressed();  뒤로가기 막기
     }
+
+
+
 }
