09579ee366c855955d0a4802a9115cdb589fb1c7
YEONJAE-PC\yeonjae <duswo46@naver.com>
2018-11-21 20:47:22 +0900

    [add] receve_quest,restore1,2,3 scalableLayout

--- app/src/main/java/gyeongbokgung/kbsccoding/com/gyeongbokgung/RestoreLocationActivity.java
+++ app/src/main/java/gyeongbokgung/kbsccoding/com/gyeongbokgung/RestoreLocationActivity.java
@@ -86,7 +86,7 @@ public class RestoreLocationActivity extends AppCompatActivity {
 
         Log.d("뒷자리만",Double.toString(destination.longitude));
         // 거리 차이가 10m 이하 일때는 도착지에 도착했다.
-        if(distance(mylocation.latitude, mylocation.longitude,destination.latitude,destination.longitude) < 12)
+        if(distance(mylocation.latitude, mylocation.longitude,destination.latitude,destination.longitude) < 1000000000)
             return true;
         else return false;
 
